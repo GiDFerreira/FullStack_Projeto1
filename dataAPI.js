dae7cf45f3ed23db90ddb8d4e6a2ef0d271524cc
GiDFerreira <giovannadferreira@alunos.utfpr.edu.br>
2024-03-25 20:15:08 -0300

    Atualização arquivo dataAPI

--- dataAPI.js
+++ dataAPI.js
@@ -1,7 +1,7 @@
 async function fetchData() {
     try {
         const disneyCharacter = document.getElementById("disneyCharacter").value.toLowerCase();
-        const response = await fetch(`https://api.disneyapi.dev/character`);
+        const response = await fetch(`https://api.disneyapi.dev/character?name=${disneyCharacter}`);
 
         if (!response.ok) {
             throw new Error("Não foi possível buscar os dados dos personagens");
@@ -10,50 +10,43 @@ async function fetchData() {
         const data = await response.json();
         const characters = data.data;
 
-        const filteredCharacters = characters.filter(character => character.name.toLowerCase().startsWith(disneyCharacter));
+        const filteredCharacters = characters.filter(character => character.name.toLowerCase());
 
         const charactersContainer = document.getElementById("charactersContainer");
         charactersContainer.innerHTML = "";
 
         if (filteredCharacters.length > 0) {
             filteredCharacters.forEach(character => {
-                // Criar elemento de personagem
                 const characterDiv = document.createElement("div");
                 characterDiv.classList.add("character");
 
-                // Adicionar nome do personagem
                 const characterNameElement = document.createElement("h3");
                 characterNameElement.textContent = character.name;
 
-                // Adicionar imagem do personagem
                 const characterImage = document.createElement("img");
                 characterImage.src = character.imageUrl;
                 characterImage.alt = character.name;
 
-                // Adicionar lista de filmes do personagem
                 const filmsListElement = document.createElement("ul");
                 const films = character.films || [];
-                if(films.length > 0){
-                    films.forEach(film => {
-                    const filmItemElement = document.createElement("li");
-                    filmItemElement.textContent = film;
-                    filmsListElement.appendChild(filmItemElement);
-                });
+                if (films.length > 0) {
+                    films.map(film => {
+                        const filmItemElement = document.createElement("li");
+                        filmItemElement.textContent = film;
+                        filmsListElement.appendChild(filmItemElement);
+                    });
                 } else {
                     const noFilmsMessage = document.createElement("p");
                     noFilmsMessage.textContent = "Esse personagem não possui filmes";
                     filmsListElement.appendChild(noFilmsMessage);
-                    
                 }
 
-                // Adicionar elementos ao container de personagens
                 characterDiv.appendChild(characterNameElement);
                 characterDiv.appendChild(characterImage);
                 characterDiv.appendChild(filmsListElement);
                 charactersContainer.appendChild(characterDiv);
             });
         } else {
-            // Nenhum personagem encontrado
             charactersContainer.textContent = "Nenhum personagem encontrado com esse nome.";
         }
     } catch (error) {
